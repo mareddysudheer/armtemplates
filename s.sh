ede8326e641907a1121d7a9360b1239f6c5b550a
mareddysudheer <43027918+mareddysudheer@users.noreply.github.com>
2018-09-18 00:29:41 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -4,4 +4,5 @@
  sudo aptget install dirmngr
  sudo aptkey adv keyserveroptions httpproxy=http://<USER>:<PASSWORD>@<PROXYHOST>:<PROXYPORT>/ keyserver packages.microsoft.com recvkeys 52E16F86FEE04B979B07E28DB02C46DF417A0893
  sudo aptget update && sudo aptget upgrade
+ 
  sudo aptget install onlyupgrade y azurecli
